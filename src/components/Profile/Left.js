121981c115a289982bef001fc6c284368dcce607
Danil <kabirdanilov14@gmail.com>
2023-10-01 20:52:08 +0500

    some fixes

--- src/components/Profile/Left.js
+++ src/components/Profile/Left.js
@@ -1,32 +1,59 @@
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+
 import Card from "../../shared/ui/Card";
 import EmptyAvatar from "../../shared/ui/EmptyAvatar";
 import TextMain from "../../shared/text/TextMain";
 import TextSecondary from "../../shared/text/TextSecondary";
+import { ButtonGhost } from "../../shared/ui/Button";
+import EditModal from "../../components/Profile/EditModal";
+
+import SettingsIcon from "../../shared/icons/SettingsIcon";
 
 const Left = ({ data }) => {
-  return (
-    <Card styles="min-w-[260px] [@media(hover)]:fixed [@media(hover)]:max-w-[260px] w-full flex flex-col gap-[12px]">
-      <EmptyAvatar />
+  const [modalState, setModalState] = useState(false);
 
-      <div className="flex flex-col gap-[6px]">
-        <TextMain
-          text={data.name}
-          styles="font-medium text-[18px] leading-[21.6px] tracking-[-0.025em]"
-        />
-        <TextSecondary
-          text={`${data.about}`}
-          styles="font-medium text-[16px] leading-[16px] tracking-[-0.015em]"
-        />
-        <TextSecondary
-          text={`${data.age} y.o.`}
-          styles="font-medium text-[14px] leading-[16px] tracking-[-0.015em]"
-        />
-        <TextSecondary
-          text={`${data.birth}`}
-          styles="font-medium text-[14px] leading-[16px] tracking-[-0.015em]"
-        />
-      </div>
-    </Card>
+  const isMobile = useMediaQuery({ query: "(pointer:coarse)" });
+  const { height, width } = useWindowDimensions();
+
+  return (
+    <>
+      <Card styles="min-w-[260px] [@media(hover)]:max-w-[260px] w-full flex flex-col gap-[12px]">
+        <EmptyAvatar />
+
+        <div className="flex flex-col gap-[6px]">
+          <TextMain
+            text={data.name}
+            styles="font-medium text-[18px] leading-[21.6px] tracking-[-0.025em]"
+          />
+          <TextSecondary
+            text={`${data.about}`}
+            styles="font-medium text-[16px] leading-[16px] tracking-[-0.015em]"
+          />
+          <TextSecondary
+            text={`${data.age} y.o.`}
+            styles="font-medium text-[14px] leading-[16px] tracking-[-0.015em]"
+          />
+          <TextSecondary
+            text={`${data.birth}`}
+            styles="font-medium text-[14px] leading-[16px] tracking-[-0.015em]"
+          />
+        </div>
+      </Card>
+
+      <Card>
+        <ButtonGhost
+          text="Редактировать профиль"
+          onClick={() => setModalState(true)}
+        >
+          <SettingsIcon />
+        </ButtonGhost>
+      </Card>
+
+      {/* modal */}
+      <EditModal state={modalState} setstate={setModalState} />
+      {/* modal */}
+    </>
   );
 };
 
